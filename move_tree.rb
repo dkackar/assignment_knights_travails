3cf07d7de590840469f8ac49dcae0703b953f81f
Deepa Kackar <dkackar99@yahoo.com>
2016-01-13 17:05:40 -0800

    Added child move structures

--- move_tree.rb
+++ move_tree.rb
@@ -1,6 +1,8 @@
 Move = Struct.new(:pos, :depth, :child, :parent)
 
 class MoveTree
+  POSSIBLE_POS = [[-1,2], [1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1],[-2,1]]
+  SIZE = 5 
 
   attr_accessor :root, :max_depth
 
@@ -9,22 +11,30 @@ class MoveTree
     @max_depth = max_depth
   end
 
-  def create_first_move(pos, max_depth)
-    @root = Move.new(pos, @max_depth, @child_pos)
+  def create_first_move(pos)
+    @root = Move.new(pos, 1, [])
   end
 
-  def create_move(pos, max_depth)
-    possible_positions(pos)
+  def create_move(current_node=nil, pos, max_depth)
+    #possible_positions(pos)
 
-    if @root.nil?
-      create_first_move(pos, @max_depth)
-    else
-
+    if current_node.nil?
+      create_first_move(pos)
+      current_node = @root
+    end 
     
-      # ignore this section
-      # current_node = @root
+    #puts "Current node is #{current_node}"
+    depth = current_node.depth + 1
 
+    possible_positions(pos).each do |valid_pos|
+      child_move = Move.new(valid_pos,depth,[],current_node)
+      current_node.child << child_move
     end
+
+    current_node.child.each do |each_child|
+      
+    end  
+
 
   end
 
@@ -32,14 +42,14 @@ class MoveTree
     @child_pos = []
     y = pos[0]
     x = pos[1]
-
-    @child_pos = [[y+2, x-1],[y+2, x+1], [y+1, x+2], 
-            [y-1, x+2], [y-2, x+1], [y-2, x-1], [y-1, x-2], [y+1, x-2]]
-
+    POSSIBLE_POS.each do |x1, y1|
+      @child_pos << [x+x1, y+y1] if (1..SIZE) === x+x1  && (1..SIZE) === y+y1
+    end
+    @child_pos
   end
 
 end
 
 tree = MoveTree.new()
 tree.create_move([3,3], 1)
-print tree.root.child
+print tree.root
